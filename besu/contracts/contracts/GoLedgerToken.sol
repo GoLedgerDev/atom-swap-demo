baa400bafbefb747fc87d46274d40608d634c6b3
Samuel Venzi <samuel.venzi@me.com>
2023-08-25 14:25:06 -0300

    Remove unnecessary folders

--- besu/contracts/contracts/GoLedgerToken.sol
+++ besu/contracts/contracts/GoLedgerToken.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GoLedgerToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("GoLedger Token", "GLDR") {
--- besu/contracts/contracts/HTLCTokenSwap.sol
+++ besu/contracts/contracts/HTLCTokenSwap.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "./ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract HTLCTokenSwap { 
 
